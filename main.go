7e1a7e2a52d26420030c96173b02a3f264385bfc
Eric Haney <haneyeric@gmail.com>
2024-12-26 18:51:21 -0500

    chirpy html

---
+++ main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"log"
+	"net/http"
+)
+
+func main() {
+	const port = "8080"
+	mux := http.NewServeMux()
+	mux.Handle("/", http.FileServer(http.Dir(".")))
+	server := &http.Server{
+		Addr: ":" + port,
+		Handler: mux,
+	}
+	log.Printf("Serving on port: %s\n", port)
+	log.Fatal(server.ListenAndServe())
+}
